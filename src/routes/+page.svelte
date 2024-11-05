8ca9b55ad515dac60a57e25e0775030efd059476
Alex Sieke <asieke@gmail.com>
2024-11-05 18:09:26 -0500

    needle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,38 +5,73 @@
 	let probability = $state(0);
 	let lastUpdated = $state(new Date());
 
-	async function getMarketPrices(): Promise<any> {
-		const MARKET_ID = '1730835757006';
-		const endpoint = `https://gamma-api.polymarket.com/markets/253591`;
-
-		try {
-			const response = await fetch(endpoint, {
-				method: 'GET',
-				headers: { 'Content-Type': 'application/json' }
-			});
-
-			if (!response.ok) {
-				throw new Error(`Error fetching market data: ${response.statusText}`);
-			}
-
-			const data = await response.json();
-			const lastPrice = data.lastTradePrice;
-
-			if (!data || !lastPrice) {
-				throw new Error('Invalid response format');
-			}
-
-			probability = Math.floor(lastPrice * 100);
-		} catch (error) {
-			console.error(error);
-			throw error;
+	const markets = $state([
+		{
+			id: '253591',
+			yes: 'Trump',
+			no: 'Kamala',
+			flip: false,
+			title: '2024 Election Winner',
+			name: 'Who will be the next president of the USA? Trump or Kamala?',
+			percentage: 0
+		},
+		{
+			id: '253727',
+			yes: 'Trump Popular Vote',
+			no: 'Kamala Popular Vote Popular Vote',
+			flip: true,
+			title: '2024 Popular Vote Winner',
+			name: 'Who will win the popular vote in the 2024 US presidential election?',
+			percentage: 0
+		},
+		{
+			id: '252298',
+			yes: 'Republican Senate',
+			no: 'Democratic Senate',
+			flip: true,
+			title: '2024 Senate Winner',
+			name: 'Who will win the US Senate in 2024?',
+			percentage: 0
+		},
+		{
+			id: '255151',
+			yes: 'Trump',
+			no: 'Kamala',
+			flip: true,
+			title: '2024 Pennsylvania Winner',
+			name: 'Who will win Pennsylvania in the 2024 US presidential election?',
+			percentage: 0
 		}
-	}
+	]);
 
-	async function manualUpdate() {
-		getMarketPrices();
-		lastUpdated = new Date();
-		console.log('updating');
+	async function getMarketPrices(): Promise<any> {
+		for (let i = 0; i < markets.length; i++) {
+			const MARKET_ID = markets[i].id;
+			const endpoint = `https://gamma-api.polymarket.com/markets/${MARKET_ID}`;
+
+			try {
+				const response = await fetch(endpoint, {
+					method: 'GET',
+					headers: { 'Content-Type': 'application/json' }
+				});
+
+				if (!response.ok) {
+					throw new Error(`Error fetching market data: ${response.statusText}`);
+				}
+
+				const data = await response.json();
+				const lastPrice = markets[i].flip ? 1 - data.lastTradePrice : data.lastTradePrice;
+
+				if (!data || !lastPrice) {
+					throw new Error('Invalid response format');
+				}
+
+				markets[i].percentage = Math.round(lastPrice * 100);
+			} catch (error) {
+				console.error(error);
+				throw error;
+			}
+		}
 	}
 
 	onMount(async () => {
@@ -44,18 +79,27 @@
 		setInterval(async () => {
 			await getMarketPrices();
 			lastUpdated = new Date();
-		}, 20000);
+		}, 60000);
 	});
 </script>
 
-<div class="prose mx-auto">
-	<h1 class="w-full pt-10 text-center font-serif">Election Needle!</h1>
-	<p class="px-12 text-center text-xl">
-		Who will be the next president of the USA? Trump or Comrade Kamala?
-	</p>
+<div class="scroll-container">
+	{#each markets as market}
+		<div class="market-section">
+			<Needle {lastUpdated} {market} />
+		</div>
+	{/each}
 </div>
-<Needle {probability} {lastUpdated} />
 
-<div class="flex w-full justify-center">
-	<button class="center bg-slate-200 p-3" onclick={manualUpdate}>Fetch Market Data</button>
-</div>
+<style>
+	.scroll-container {
+		scroll-snap-type: y mandatory;
+		overflow-y: scroll;
+		height: 100vh;
+	}
+
+	.market-section {
+		scroll-snap-align: start;
+		height: 100vh;
+	}
+</style>
--- src/routes/Needle.svelte
+++ src/routes/Needle.svelte
@@ -1,13 +1,23 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
-	let { probability, lastUpdated }: { probability: number; lastUpdated: Date } = $props();
+	type Market = {
+		id: string;
+		yes: string;
+		title: string;
+		flip: boolean;
+		no: string;
+		name: string;
+		percentage: number;
+	};
+
+	let { lastUpdated, market }: { lastUpdated: Date; market: Market } = $props();
 
 	//if probability = 100 then deg = 90
 	//if probability = 0 then deg = -90
-	let deg = $derived((probability / 100) * 180 - 90);
+	let deg = $derived((market.percentage / 100) * 180 - 90);
 
-	let color = $derived(probability > 50 ? '#c93136ff' : '#1675b7ff');
+	let color = $derived(market.percentage > 50 ? '#c93136ff' : '#1675b7ff');
 
 	let size = $state(0);
 	let bubbleSize = $state(24);
@@ -20,83 +30,92 @@
 	});
 </script>
 
-{#if size > 0}
-	<div
-		class="guage relative mx-auto mt-20"
-		style={`width: ${size}px; height: ${size}px; border-radius: ${size / 2}px ${size / 2}px 0 0;`}
-	>
-		<div class="absolute z-50 text-center" style={`top: ${size / 2 + 32}px; width: ${size}px;`}>
-			{#if probability > 50}
-				<h1 class="font-serif text-3xl font-bold" style="color: {color}">Trump: {probability}%</h1>
-			{:else}
-				<h1 class="font-serif text-3xl" style="color: {color}">
-					Comrade Kamala: {100 - probability}%
-				</h1>
-			{/if}
-			<h3 class="pt-2 text-slate-400">
-				Last Updated: {lastUpdated.toLocaleString()}
-			</h3>
+{#if market.percentage > 0}
+	<div class="h-[100vh] overflow-hidden">
+		<div class="prose mx-auto">
+			<h1 class="w-full pt-10 text-center font-serif">{market.title}</h1>
+			<p class="px-12 text-center text-2xl">{market.name}</p>
 		</div>
 		<div
-			class="absolute z-20 rounded-full bg-white"
-			style={`width: ${carveoutSize}px; height: ${carveoutSize}px; top: ${size / 2 - carveoutSize / 2}px; left: ${size / 2 - carveoutSize / 2}px`}
-		></div>
-		<div
-			class="absolute text-center text-xs md:text-lg"
-			style="width: {size * 0.2}px; top: {size / 2.5}px; left: {size / 24}px"
+			class="guage relative mx-auto mt-20"
+			style={`width: ${size}px; height: ${size}px; border-radius: ${size / 2}px ${size / 2}px 0 0;`}
 		>
-			Very Likely
-		</div>
-		<div
-			class="absolute text-center text-xs md:text-lg"
-			style="width: {size * 0.2}px; top: {size / 4}px; left: {size / 10}px"
-		>
-			Likely
-		</div>
-		<div
-			class="absolute text-center text-xs md:text-lg"
-			style="width: {size * 0.2}px; top: {size / 7}px; left: {size / 4.5}px"
-		>
-			Leaning
-		</div>
-		<div
-			class="absolute text-center text-xs md:text-lg"
-			style="width: {size * 0.2}px; top: {size / 15}px; left: {size / 2 - size * 0.1}px"
-		>
-			Tossup
-		</div>
-		<div
-			class="absolute text-center text-xs md:text-lg"
-			style="width: {size * 0.2}px; top: {size / 7}px; right: {size / 4.5}px"
-		>
-			Leaning
-		</div>
-		<div
-			class="absolute text-center text-xs md:text-lg"
-			style="width: {size * 0.2}px; top: {size / 4}px; right: {size / 10}px"
-		>
-			Likely
-		</div>
-		<div
-			class="absolute text-center text-xs md:text-lg"
-			style="width: {size * 0.2}px; top: {size / 2.5}px; right: {size / 25}px"
-		>
-			Very Likely
-		</div>
+			<div class="absolute z-50 text-center" style={`top: ${size / 2 + 32}px; width: ${size}px;`}>
+				{#if market.percentage > 50}
+					<h1 class="font-serif text-3xl font-bold" style="color: {color}">
+						{market.yes}: {market.percentage}%
+					</h1>
+				{:else}
+					<h1 class="font-serif text-3xl" style="color: {color}">
+						{market.no}
+						{100 - market.percentage}%
+					</h1>
+				{/if}
+				<h3 class="pt-2 text-slate-400">
+					Last Updated: {lastUpdated.toLocaleString()}
+				</h3>
+			</div>
+			<div
+				class="absolute z-20 rounded-full bg-white"
+				style={`width: ${carveoutSize}px; height: ${carveoutSize}px; top: ${size / 2 - carveoutSize / 2}px; left: ${size / 2 - carveoutSize / 2}px`}
+			></div>
+			<div
+				class="absolute text-center text-xs md:text-lg"
+				style="width: {size * 0.2}px; top: {size / 2.5}px; left: {size / 24}px"
+			>
+				Very Likely
+			</div>
+			<div
+				class="absolute text-center text-xs md:text-lg"
+				style="width: {size * 0.2}px; top: {size / 4}px; left: {size / 10}px"
+			>
+				Likely
+			</div>
+			<div
+				class="absolute text-center text-xs md:text-lg"
+				style="width: {size * 0.2}px; top: {size / 7}px; left: {size / 4.5}px"
+			>
+				Leaning
+			</div>
+			<div
+				class="absolute text-center text-xs md:text-lg"
+				style="width: {size * 0.2}px; top: {size / 15}px; left: {size / 2 - size * 0.1}px"
+			>
+				Tossup
+			</div>
+			<div
+				class="absolute text-center text-xs md:text-lg"
+				style="width: {size * 0.2}px; top: {size / 7}px; right: {size / 4.5}px"
+			>
+				Leaning
+			</div>
+			<div
+				class="absolute text-center text-xs md:text-lg"
+				style="width: {size * 0.2}px; top: {size / 4}px; right: {size / 10}px"
+			>
+				Likely
+			</div>
+			<div
+				class="absolute text-center text-xs md:text-lg"
+				style="width: {size * 0.2}px; top: {size / 2.5}px; right: {size / 25}px"
+			>
+				Very Likely
+			</div>
 
-		<!-- Needle-->
-		<div
-			class="needle absolute z-40"
-			style={`transform: rotate(${deg}deg); top: -${needleWidth / 2}px; left:${(size - needleWidth) / 2}px; height: ${
-				size + needleWidth
-			}px; width: ${needleWidth}px; background: linear-gradient(to bottom, ${color} 50%, transparent 50%);`}
-		></div>
+			<!-- Needle-->
+			<div
+				class="needle absolute z-40"
+				style={`transform: rotate(${deg}deg); top: -${needleWidth / 2}px; left:${(size - needleWidth) / 2}px; height: ${
+					size + needleWidth
+				}px; width: ${needleWidth}px; background: linear-gradient(to bottom, ${color} 50%, transparent 50%);`}
+			></div>
 
-		<div class="absolute z-30 h-[2px] w-full bg-black" style="top: {size / 2}px;"></div>
-		<div
-			class="absolute z-50 rounded-full border-[1px] border-white"
-			style={`background-color: ${color}; width: ${bubbleSize}px; height: ${bubbleSize}px; left: ${size / 2 - bubbleSize / 2}px; top: ${size / 2 - bubbleSize / 2}px`}
-		></div>
+			<div class="absolute z-30 h-[2px] w-full bg-black" style="top: {size / 2}px;"></div>
+			<div
+				class="absolute z-50 rounded-full border-[1px] border-white"
+				style={`background-color: ${color}; width: ${bubbleSize}px; height: ${bubbleSize}px; left: ${size / 2 - bubbleSize / 2}px; top: ${size / 2 - bubbleSize / 2}px`}
+			></div>
+		</div>
 	</div>
 {/if}
 
